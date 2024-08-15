4a0d36ea4c9e99d85faa63edd5d531918bdf2762
Pedro Victor <pedrovictor.rveras12@gmail.com>
2024-08-14 23:37:42 -0300

    up

--- app.py
+++ app.py
@@ -282,8 +282,8 @@ class ChatbotServer:
                 }
             )
 
-        app.run(debug=True)
-
+        #app.run(debug=True)
+        app.run(host='0.0.0.0', port=8080)
 
 if __name__ == "__main__":
     server = ChatbotServer()
