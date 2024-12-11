991f6def86bcc296f54c42bdb95793dc35f917f4
Pedro Victor <pedrovictor.rveras12@gmail.com>
2024-12-11 19:40:02 -0300

    up

---
+++ c3po/free_speach_text.py
@@ -0,0 +1,15 @@
+
+
+url = "https://crikk.com/text-to-speech/portuguese/"
+
+# aprender a usar o selenium ou playwrith para consumir a pagina web e fechar e sair o som com performace 
+
+
+# como fazer automacao rapida de chatbot
+
+# arrumar o markdwon e usar botoes para gerar algo quando disponivel na memoria
+
+# vericar link dos 5 frameworks web agentees e usar 2 projetos no linkedin com isso
+
+
+
